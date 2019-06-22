07e7a36202057f02eef776b7c4974835f9e6e404
Alex Lisenko <detoner777@gmail.com>
2019-06-22 15:41:41 +0300

    Register component in develop

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,13 +1,74 @@
 import React from 'react';
+import firebase from '../../firebase';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
 
 class Register extends React.Component {
-    state={}
+    state={
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirmation: '',
+        errors: []
+    };
+    
+    isFormValid = () => {
+        let errors = [];
+        let error;
 
-    handleChange = () => {}
+        if (this.isFormEmpty(this.state)) {
+            error = { message: 'Fill in all fields' };
+            this.setState( {errors: errors.concat(error)} );
+            return false;
+        } else if (!this.isPassswordValid(this.state)){
+            error = { message: 'Password is ivalid' };
+            this.setState({ errors: errors.concat(error)});
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    isPassswordValid = ({ password, passwordConfirmation }) => {
+            if(password.lengs < 6 || passwordConfirmation.length < 6) {
+                return false;
+            } else if (password !== passwordConfirmation) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+    isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return !username.length || !email.length || !password.length ||
+        !passwordConfirmation.length;
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    
+
+     handleSubmit = event => {
+         if (this.isFormValid()) {
+         event.preventDefault();
+         firebase
+              .auth()
+              .createUserWithEmailAndPassword(this.state.email, this.state.password)
+              .then(createdUser => {
+                  console.log(createdUser);
+              })
+              .catch(err => {
+                  console.error(err);
+              });
+            };
+            };
 
     render() {
+        const { username, email, password, passwordConfirmation, errors } = this.state;
+
         return (
         
             <Grid textAlign="center" verticalAlign="middle" className="app">
@@ -16,21 +77,24 @@ class Register extends React.Component {
                 <Icon name="puzzle piece" color="orange" />
                 Register for DevChat
              </Header>
-             <Form size="large">
+             <Form onSubmit={this.handleSubmit} size="large">
                  <Segment stacked>
                      <Form.Input fluid name="username" icon="user" iconPosition="left" 
-                      placeholder="Username" onChange={this.handleChange} type="text" />
+                      placeholder="Username" onChange={this.handleChange} type="text" 
+                      value={username} />
 
                       <Form.Input fluid name="email" icon="mail" iconPosition="left" 
                       placeholder="Email Address" onChange={this.handleChange} 
-                      type="email" />
+                      type="email" value={email} />
 
                       <Form.Input fluid name="password" icon="lock" iconPosition="left" 
-                      placeholder="Password" onChange={this.handleChange} type="password" />
+                      placeholder="Password" onChange={this.handleChange} type="password"
+                      value={password} />
 
                       
                       <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" 
-                      placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+                      placeholder="Password Confirmation" onChange={this.handleChange} type="password" 
+                      value={passwordConfirmation}/>
 
                      
                         <Button color="orange" fluid size0="large">Submit</Button>
@@ -38,6 +102,12 @@ class Register extends React.Component {
  
                 </Segment>       
                 </Form>
+                {errors.length > 0 && (
+                    <Message error>
+                        <h3>Error</h3>
+                        {this.displayErrors( errors)}
+                    </Message>
+                )}
                  <Message>Already a user?<Link to="/login">Login</Link>
                  </Message>
                 </Grid.Column>
