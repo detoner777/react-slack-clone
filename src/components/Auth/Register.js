718a50fd89dfb344fe99d24c091aed713855f313
Alex Lisenko <detoner777@gmail.com>
2019-06-23 11:59:43 +0300

    Errors heandling on Register component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -9,7 +9,9 @@ class Register extends React.Component {
         email: '',
         password: '',
         passwordConfirmation: '',
-        errors: []
+        errors: [],
+        loading: false
+
     };
     
     isFormValid = () => {
@@ -52,22 +54,41 @@ class Register extends React.Component {
     
 
      handleSubmit = event => {
-         if (this.isFormValid()) {
-         event.preventDefault();
-         firebase
+        event.preventDefault();
+        if (this.isFormValid()) {
+
+          this.setState( { errors:[], loading: true } );
+          firebase
               .auth()
               .createUserWithEmailAndPassword(this.state.email, this.state.password)
               .then(createdUser => {
                   console.log(createdUser);
+                  this.setState( {loading: false } );
               })
               .catch(err => {
                   console.error(err);
+                  this.setState( { errors: this.state.errors.concat(err), loading: false} );
               });
-            };
+            }
             };
 
+       handleInputError = (errors, inputName) => {
+        return errors.some(error =>
+             error.message.toLowerCase().includes(inputName)
+             ) 
+             ? 'error' 
+             : ''
+          }
+
     render() {
-        const { username, email, password, passwordConfirmation, errors } = this.state;
+        const { 
+            username, 
+            email, 
+            password, 
+            passwordConfirmation, 
+            errors, 
+            loading 
+        } = this.state;
 
         return (
         
@@ -85,19 +106,25 @@ class Register extends React.Component {
 
                       <Form.Input fluid name="email" icon="mail" iconPosition="left" 
                       placeholder="Email Address" onChange={this.handleChange} 
-                      type="email" value={email} />
+                      type="email" value={email} 
+                      className={this.handleInputError(errors, 'email')} />
 
                       <Form.Input fluid name="password" icon="lock" iconPosition="left" 
                       placeholder="Password" onChange={this.handleChange} type="password"
-                      value={password} />
+                      value={password}
+                      className={this.handleInputError(errors, 'password')} />
 
                       
                       <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" 
                       placeholder="Password Confirmation" onChange={this.handleChange} type="password" 
-                      value={passwordConfirmation}/>
+                      value={passwordConfirmation}
+                      className={this.handleInputError(errors, 'password')}/>
 
                      
-                        <Button color="orange" fluid size0="large">Submit</Button>
+                        <Button disabled={loading} className={loading ? 'loading' : ''} 
+                        color="orange" fluid size="large">
+                            Submit
+                        </Button>
   
  
                 </Segment>       
