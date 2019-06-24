b78d12b6f3237beb578f693f7df46018cb7feeda
Alexey_Lisenko <detoner777@gmail.com>
2019-06-24 18:41:33 +0300

    Set up Redux to make User data avaible to global state

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,18 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD-EqlDa5KbQE2RZJIQ7mSapNb2HylJbQs",
+  authDomain: "react-slack-clone-18f3a.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-18f3a.firebaseio.com",
+  projectId: "react-slack-clone-18f3a",
+  storageBucket: "react-slack-clone-18f3a.appspot.com",
+  messagingSenderId: "840688392461",
+  appId: "1:840688392461:web:46d5ff6d17483074"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-
- let firebaseConfig = {
-    apiKey: "AIzaSyD-EqlDa5KbQE2RZJIQ7mSapNb2HylJbQs",
-    authDomain: "react-slack-clone-18f3a.firebaseapp.com",
-    databaseURL: "https://react-slack-clone-18f3a.firebaseio.com",
-    projectId: "react-slack-clone-18f3a",
-    storageBucket: "react-slack-clone-18f3a.appspot.com",
-    messagingSenderId: "840688392461",
-    appId: "1:840688392461:web:46d5ff6d17483074"
-  };
-  firebase.initializeApp(firebaseConfig);
-
-  export default firebase;
+export default firebase;
