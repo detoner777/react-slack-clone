7acdcdac28a65fafb8bc886b595b4e70f6985a80
Alexey_Lisenko <detoner777@gmail.com>
2019-07-11 10:16:50 +0300

    Develop Avatardownload component

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image, Modal, Input, Button } from 'semantic-ui-react';
 import firebase from '../../firebase';
 
 
 class UserPanel extends React.Component {
     state = {
-        user: this.props.currentUser
-    }
+        user: this.props.currentUser,
+        modal: false
+    };
 
+    openModal = () => this.setState({ modal: true });
+
+    closeModal = () => this.setState({ modal: false });
 
     dropdownOptions = () => [
-        { 
+        {
             key: 'user',
             text: <span>
-                    Signed in as<strong>{this.state.user.displayName}</strong>
-                 </span>,
+                Signed in as<strong>{this.state.user.displayName}</strong>
+            </span>,
             disabled: true
         },
         {
             key: 'avatar',
-            text: <span>Change Avatar</span>
+            text: <span onClick={this.openModal}>Change Avatar</span>
         },
-        {   
+        {
             key: 'signout',
             text: <span onClick={this.handleSignout}>Sign Out</span>
         }
@@ -31,35 +35,70 @@ class UserPanel extends React.Component {
         firebase
             .auth()
             .signOut()
-            .then(()=> console.log('signed out!'))
+            .then(() => console.log('signed out!'))
     }
 
     render() {
 
-        const { user } = this.state;
-            
+        const { user, modal } = this.state;
+        const { primaryColor } = this.props;
 
-        return(
-         <Grid style= {{ background: "#4c3c4c" }}>
-             <Grid.Column>
-                 <Grid.Row style= {{ padding: '1.2em', margin: 0 }}>
-                    {/*App Header */}
-                    <Header inverted floated="left" as="h2">
-                     <Icon name="code" />
-                     <Header.Content>DevChat</Header.Content>       
-                    </Header>   
-                </Grid.Row>
-                    {/*User Dropdown*/}
-                <Header style = {{ padding: '0.25' }} as="h4" inverted>
-                    <Dropdown trigger= {
-                        <span>
-                         <Image src={user.photoURL} spaced="right" avatar/>   
-                        {user.displayName}
-                        </span>
-                    } options={this.dropdownOptions()} />
-                </Header>     
-             </Grid.Column>
-         </Grid>           
+        return (
+            <Grid style={{ background: primaryColor }}>
+                <Grid.Column>
+                    <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
+                        {/*App Header */}
+                        <Header inverted floated="left" as="h2">
+                            <Icon name="code" />
+                            <Header.Content>DevChat</Header.Content>
+                        </Header>
+
+                        {/*User Dropdown*/}
+                        <Header style={{ padding: '0.25' }} as="h4" inverted>
+                            <Dropdown trigger={
+                                <span>
+                                    <Image src={user.photoURL} spaced="right" avatar />
+                                    {user.displayName}
+                                </span>
+                            } options={this.dropdownOptions()} />
+                        </Header>
+                    </Grid.Row>
+                    {/* Change User Avatar Maodal */}
+                    <Modal basic open={modal} onClose={this.closeModal}>
+                        <Modal.Header>Change Avatar</Modal.Header>
+                        <Modal.Content>
+                            <Input
+                                fluid
+                                type='file'
+                                label='New Avatar'
+                                name='previewImage'
+                            />
+                            <Grid centered stackable columns={2}>
+                                <Grid.Row centered>
+                                    <Grid.Column className='ui center aligned grid'>
+                                        {/* Image Preview */}
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        {/* Cropped Image Preview */}
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Modal.Content>
+                        <Modal.Actions>
+                            <Button color="green" inverted>
+                                <Icon name="save" />Change Avatar
+                            </Button>
+                            <Button color="grey" inverted>
+                                <Icon name="image" />Preview
+                            </Button>
+                            <Button color="red" inverted onClick={this.closeModal}>
+                                <Icon name="remove" />Cancel
+                            </Button>
+                        </Modal.Actions>
+                    </Modal>
+
+                </Grid.Column>
+            </Grid>
 
 
         )
