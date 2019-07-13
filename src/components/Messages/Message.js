5fde3496a90cde707f12b0a3fc6fd58e777c6605
Alex Lisenko <detoner777@gmail.com>
2019-07-13 18:18:56 +0300

    Complit previous task

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -22,7 +22,7 @@ const Message = ({ message, user }) => (
              {isImage(message) ? 
                <Image src={message.image} className="message__image"/> :
                <Comment.Text>{message.content}</Comment.Text> 
-                 }
+               }
         </Comment.Content>
    </Comment>
 );
