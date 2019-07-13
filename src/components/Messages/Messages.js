67b96504990014734b63ebef1a1355b612f977c2
Alexey_Lisenko <detoner777@gmail.com>
2019-07-13 14:06:08 +0300

    Typing component +CSS done

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -7,6 +7,7 @@ import { setUserPosts } from '../../actions/index';
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
 import Message from "./Message";
+import Typing from './Typing';
 
 class Messages extends React.Component {
   state = {
@@ -192,6 +193,9 @@ class Messages extends React.Component {
             {searchTerm
               ? this.displayMessages(searchResults)
               : this.displayMessages(messages)}
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <span className="user__typing">Alena is typing</span> <Typing />
+            </div>
           </Comment.Group>
         </Segment>
 
